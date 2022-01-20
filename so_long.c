12558326f85e99e1ce857dceb01b4a594c5e4e7b
Fahima BOUMELLIL <fboumell@e1r3p1.42.fr>
2022-01-20 17:59:33 +0100

    a

--- so_long.c
+++ so_long.c
@@ -6,30 +6,27 @@
 /*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 14:39:28 by fboumell          #+#    #+#             */
-/*   Updated: 2022/01/20 16:39:25 by fboumell         ###   ########.fr       */
+/*   Updated: 2022/01/20 17:47:14 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	check_arg(int ac, char *av)
+int	check_arg(int ac, char *av) //voir clg
 {
 	int i;
 	int	len;
-	int	fd;
 
 	i = 0;
 	len = ft_strlen(av);
-	if (ac != 2)
+	if (ac != 2)  //attention segfault quand ac == 1!!!
 	{
 		if (ac < 2)
-			printf("Missing one argument\n");
+			printf("Error\nMissing one argument\n");
 		if (ac > 2)
-			printf("Too many arguments\n");
+			printf("Error\nToo many arguments\n");
 		exit(0);
 	}
-	fd = open(av, O_RDONLY);
-	close(fd);
 	if (av[len - 3] != 'b' && av[len - 2] != 'e' && av[len - 1] != 'r' && av[len - 4] != '.')
 	{
 		printf("Wrong extension, argument must be a .ber file\n");
