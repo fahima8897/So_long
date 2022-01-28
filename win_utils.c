93bbdfbd1636d4f86ca38e4c5269a5c18ca8171b
Fahima BOUMELLIL <fboumell@e3r3p1.clusters.42paris.fr>
2022-01-28 19:53:42 +0100

    a

--- win_utils.c
+++ win_utils.c
@@ -6,7 +6,7 @@
 /*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 15:20:35 by fboumell          #+#    #+#             */
-/*   Updated: 2022/01/28 17:14:41 by fboumell         ###   ########.fr       */
+/*   Updated: 2022/01/28 19:44:04 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	ft_init_window(t_data *data)
 
 void	ft_loop(t_data data)
 {
-//	mlx_loop_hook(data.mlx, &ft_image, &data);
+	mlx_loop_hook(data.mlx, &ft_image, &data);
 	mlx_hook(data.mlx_win, 2, 1L << 0, &ft_close_escape, &data);
 	mlx_hook(data.mlx_win, 17, 0L, &ft_red_cross, &data);
 	mlx_loop(data.mlx);
